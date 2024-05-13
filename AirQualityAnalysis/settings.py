c3d44b22e67dc737999a7a466d0cbdc7fef5d2b0
Sakunoakarinn <43726721+actredphos2017@users.noreply.github.com>
2024-05-13 10:01:59 +0800

    修改:改数据库为 Sqlite3

--- AirQualityAnalysis/settings.py
+++ AirQualityAnalysis/settings.py
@@ -86,14 +86,21 @@ WSGI_APPLICATION = 'AirQualityAnalysis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'AirQualityAnalysisDatabase',
+#         'USER': 'root',
+#         'PASSWORD': 'gamemode1',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'AirQualityAnalysisDatabase',
-        'USER': 'root',
-        'PASSWORD': 'gamemode1',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # 数据库文件路径
     }
 }
 
